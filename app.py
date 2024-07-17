177f73bd0dac78521ecb3e78494271a1f3d0551d
Nilaksh Kashyap <nilakshsk003@gmail.com>
2024-07-18 03:51:04 +0530

    resolving errors

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-# Before running the code run `pip install -r requirements.txt` in the terminal
-
 import streamlit as st
 import google.generativeai as palm
 from dotenv import load_dotenv
-import os
+import os,sys
+from google.api_core.exceptions import DeadlineExceeded
 
 #Load the .env file
 load_dotenv()
@@ -16,52 +15,141 @@ palm.configure(api_key=gemini_api_key)
 
 # Define the model to use
 model_name = "models/chat-bison-001"
+languages = ["Python", "Java", "C++"]
 
 
 # Function to translate code from one language to another
 def translate_code(code_snippet, target_language):
+    instruction = "Give Fully runnable code. Include syntax highlighting. Give only the translated code"
     prompt = f"Translate the following code to {target_language}:\n\n{code_snippet}"
-    response = palm.chat(
-        model=model_name,
-        messages=[prompt]
-    )
+    try:
+        response = palm.chat(
+            model=model_name,
+            messages=[instruction,prompt]
+        )
+    except DeadlineExceeded:
+        print("Timeout. Please Try again")
+        exit()
     return response.candidates[0]["content"]
 
+def commandLine(targetLanguage):
+    inputFileName = outputFileName = ""
+    if len(sys.argv) < 4:
+        outputFileName = "converted.txt"
+    else:
+        outputFileName = sys.argv[3]
+    inputFileName = sys.argv[2]
+    code = ""
+    with open(inputFileName,"r") as inputFile:
+        code = inputFile.read()
+
+    with open(outputFileName,"w") as outputFile:
+        print("\rConverting...")
+        translated_code = translate_code(code,targetLanguage)
+        print("\rConverted...")
+        print("\rSaving...")
+        outputFile.write(translated_code)
+        print("\rOutput Saved...\n\n")
+        print(f"{targetLanguage.title()} Code:")
+        print(translated_code)
 
 # Streamlit application
 def main():
+
     st.title("CodeXchange: Ai-Powered Code Translation Tool")
+    st.write("""
+             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+             """,unsafe_allow_html=True)
+    st.markdown(
+        """
+        <style>
 
-    st.markdown("""
-    <h2>Project Description</h2>
-    <p>CodeXchange is an innovative web application designed to streamline code translation and facilitate seamless collaboration among developers working with different programming languages. Whether you're transitioning applications between platforms, collaborating in multilingual teams, or reusing code across projects, CodeXchange empowers developers to effortlessly translate code snippets between various programming languages. Leveraging advanced translation algorithms and syntax analysis, CodeXchange ensures accurate and reliable code conversion while preserving the original functionality and logic. With its intuitive interface and comprehensive language support, CodeXchange revolutionizes the development workflow, enabling teams to work together efficiently, enhance code reusability, and accelerate project delivery.</p>
-    """, unsafe_allow_html=True)
 
-    st.markdown("""
-    <h2>Scenario 1: Platform Transition</h2>
-    <p>CodeXchange assists developers in transitioning applications from one platform to another. For instance, a team working on an application written in Python needs to migrate it to Java to leverage Java's robustness and scalability in an enterprise environment. By inputting the Python code snippets and selecting Java as the target language, developers receive accurately translated code that maintains the original functionality, streamlining the migration process and minimizing the risk of introducing errors.</p>
-    """, unsafe_allow_html=True)
+        .main .block-container {
+        max-width: 1000px;
+        padding: 1rem;
+    }
 
-    st.markdown("""
-    <h2>Scenario 2: Multilingual Collaboration</h2>
-    <p>In a collaborative project where team members use different programming languages, CodeXchange facilitates seamless integration by translating code snippets as needed. Suppose one part of the team is proficient in C++ while another prefers Python. Developers can write code in their preferred language and use CodeXchange to translate it, ensuring all team members can work together efficiently without being constrained by language barriers. This enhances productivity and reduces the learning curve associated with adopting new languages.</p>
-    """, unsafe_allow_html=True)
+    .stSelectbox{
+        max-width: 25%
+    }
 
-    st.markdown("""
-    <h2>Scenario 3: Code Reusability Across Projects</h2>
-    <p>CodeXchange promotes code reusability by enabling developers to translate existing code into different languages for new projects. For example, a developer has written a set of utility functions in Java that would be beneficial for a new project being developed in C++. By translating these Java functions into C++ using CodeXchange, the developer can quickly integrate proven code into the new project, saving time and ensuring consistency across different projects.</p>
-    """, unsafe_allow_html=True)
+        .use-case-container{
+    min-height: 20vh;
+    display: flex;
+    justify-content: space-evenly;
+    font-family: "Source Sans Pro", sans-serif;
+}
 
-    st.subheader("Code Translation")
+.use-case-item{
+    flex-grow: 1;
+    margin: 48px auto;
+    margin-right: 16px;
+    padding: 16px;
+    color: rgba(245, 245, 245, 0.925);
+    border: 2px solid rgb(59, 165, 165);
+    box-shadow: 5px 5px rgba(0, 255, 255, 0.658);
+}
+
+.use-case-title{
+    font-weight: 800;
+    font-size: 24px;
+}
+.use-case-description{
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 2rem;
+    color: #9e9d9d;
+}
+        """, unsafe_allow_html=True
+    )
+
+    st.markdown(
+        f"""
+        <div class="use-case-container">
+        <div class="use-case-item">
+            <div class="use-case-title">
+                <p>Platform Transition</p>
+                <span class="material-symbols-outlined"> compare_arrows</span>
+            </div>
+            <div class="use-case-description">
+                <p>Easily migrate your codebase from one programming language to another, ensuring seamless platform transition and reducing development time.</p>
+            </div>
+        </div>
+        <div class="use-case-item">
+            <div class="use-case-title">
+                <p>Multilingual Collaboration</p>
+                <span class="material-symbols-outlined">groups</span>
+            </div>
+            <div class="use-case-description">
+                <p>Facilitate collaboration among developers who prefer different programming languages by translating code, enabling a more inclusive and productive team environment</p>
+            </div>
+        </div>
+        <div class="use-case-item">
+            <div class="use-case-title">
+                <p>Code Reusability</p>
+                <span class="material-symbols-outlined">cycle</span>
+            </div>
+            <div class="use-case-description">
+                <p>Maximize code reuse by translating existing code into different languages for various projects, ensuring consistency and saving time on development.</p>
+            </div>
+        </div>
+    </div>
+        """, unsafe_allow_html=True
+    )
+
+    
+
     source_code_snippet = st.text_area("Enter the code snippet you want to translate:")
-    target_language = st.selectbox("Select the target programming language:", ["Python", "Java", "C++"])
-    if st.button("Translation Code"):
+    target_language = st.selectbox("Select the target programming language:",languages)
+    if st.button("Translate"):
         if source_code_snippet.strip():
             with st.spinner("Translating code..."):
                 try:
                     translated_code = translate_code(source_code_snippet, target_language)
+                    print(translated_code)
                     st.success("Code translation complete!")
-                    st.code(translated_code, language=target_language.lower())
+                    st.markdown(translated_code)
                 except Exception as e:
                     st.error(f"Error translating code: {e}")
         else:
@@ -69,5 +157,13 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:
+        main()
+    elif len(sys.argv) < 3 or sys.argv[1] == "help" :
+        print("Usage: [Target Language] [Input File] [Output File]")
+        print("Languages: \n")
+        for i in languages:
+            print(f"[>] {i}")
+    elif sys.argv[1].title() in languages:
+        commandLine(sys.argv[1])
     
